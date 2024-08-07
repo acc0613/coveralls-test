03b0034fdd49bec2466ecb80385a6b135cda53ca
acc0613 <abicmia@gmail.com>
2024-08-07 21:13:56 +0800

    update test files

--- TestApp/TestAppTests/SignUpTests.swift
+++ TestApp/TestAppTests/SignUpTests.swift
@@ -9,27 +9,27 @@ import XCTest
 
 class SignUpTests: XCTestCase {
 
-    func testSignUpFormModel_WhenCreated_EmailShouldHaveValidFormat() {
-     
-        // Arrange (GIVEN)
-        let firstName = "Maksim"
-        let lastName = "Kalik"
-        let email = "test@test.com"
-        let password = "qwerty123"
-        let repeatPassword = "qwerty123"
-
-        let signUpFormModel = SignUpFormModel(
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            password: password,
-            repeatPassword: repeatPassword
-        )
-
-        // Act (WHEN)
-        let isEmailFormatValid = signUpFormModel.isValidEmailFormat()
-     
-        // Assert (THEN)
-        XCTAssertTrue(isEmailFormatValid, "Provided valid email address does not have a valid format")
-    }
+//    func testSignUpFormModel_WhenCreated_EmailShouldHaveValidFormat() {
+//     
+//        // Arrange (GIVEN)
+//        let firstName = "Maksim"
+//        let lastName = "Kalik"
+//        let email = "test@test.com"
+//        let password = "qwerty123"
+//        let repeatPassword = "qwerty123"
+//
+//        let signUpFormModel = SignUpFormModel(
+//            firstName: firstName,
+//            lastName: lastName,
+//            email: email,
+//            password: password,
+//            repeatPassword: repeatPassword
+//        )
+//
+//        // Act (WHEN)
+//        let isEmailFormatValid = signUpFormModel.isValidEmailFormat()
+//     
+//        // Assert (THEN)
+//        XCTAssertTrue(isEmailFormatValid, "Provided valid email address does not have a valid format")
+//    }
 }
