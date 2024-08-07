5ded9e9da4ab16ba25940367a0d4a0474942b3d2
acc0613 <abicmia@gmail.com>
2024-08-07 22:39:08 +0800

    add view

---
+++ TestApp/TestApp/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  TestApp
+//
+//  Created by Casabuena, Abigail (Cognizant) on 8/7/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, SwiftUI!")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
