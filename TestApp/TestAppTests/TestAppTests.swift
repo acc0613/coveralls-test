ff55b8c961fde5ea7f469d210615529e8aabd055
acc0613 <abicmia@gmail.com>
2024-08-07 21:40:50 +0800

    move func test

--- TestApp/TestAppTests/TestAppTests.swift
+++ TestApp/TestAppTests/TestAppTests.swift
@@ -19,29 +19,13 @@ final class TestAppTests: XCTestCase {
     }
 
     func testExample() throws {
-        XCTAssertTrue(true)
+        // XCTAssertTrue(true)
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-    
-//    func testExample1() throws {
-//        let expr1 = "expression 1"
-//        let expr2 = expr1
-//        XCTAssertEqual(expr1, expr2, "Expressions are equal")
-//    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-      // Put the code you want to measure the time of here.
-        }
-    }
-
-    func testSignUpFormModel_WhenCreated_EmailShouldHaveValidFormat() {
-     
+        
         // Arrange (GIVEN)
         let firstName = "Maksim"
         let lastName = "Kalik"
@@ -62,5 +46,43 @@ final class TestAppTests: XCTestCase {
      
         // Assert (THEN)
         XCTAssertTrue(isEmailFormatValid, "Provided valid email address does not have a valid format")
+        XCTAssertTrue(isEmailFormatValid, "Provided valid email address does not have a valid format")
     }
+    
+//    func testExample1() throws {
+//        let expr1 = "expression 1"
+//        let expr2 = expr1
+//        XCTAssertEqual(expr1, expr2, "Expressions are equal")
+//    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+      // Put the code you want to measure the time of here.
+        }
+    }
+
+//    func testSignUpFormModel_WhenCreated_EmailShouldHaveValidFormat() {
+//     
+//        // Arrange (GIVEN)
+//        let firstName = "Maksim"
+//        let lastName = "Kalik"
+//        let email = "test@test.com"
+//        let password = "qwerty123"
+//        let repeatPassword = "qwerty123"
+//
+//        let signUpFormModel = SignUpFormModel(
+//            firstName: firstName,
+//            lastName: lastName,
+//            email: email,
+//            password: password,
+//            repeatPassword: repeatPassword
+//        )
+//
+//        // Act (WHEN)
+//        let isEmailFormatValid = signUpFormModel.isValidEmailFormat()
+//     
+//        // Assert (THEN)
+//        XCTAssertTrue(isEmailFormatValid, "Provided valid email address does not have a valid format")
+//    }
 }
